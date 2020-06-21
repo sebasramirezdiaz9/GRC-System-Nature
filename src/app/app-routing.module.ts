b14f6225655573907dd9a862af470ac116e9463a
diana <al234644@edu.uaa.mx>
2020-06-20 19:42:21 -0500

    i did the web page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent } from './web-page/home/home.component';
+import {AboutComponent} from './web-page/about/about.component';
+import {ContactComponent } from './web-page/contact/contact.component';
+import { QuestionsComponent} from './web-page/questions/questions.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent },
+  {path:'about', component:AboutComponent },
+  {path:'contact', component:ContactComponent  },
+  {path:'questions', component: QuestionsComponent},
+  {path: '**', pathMatch: 'full',redirectTo: 'home'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { NavbarComponent } from './web-page/navbar/navbar.component';
+import { HomeComponent } from './web-page/home/home.component';
+import { QuestionsComponent } from './web-page/questions/questions.component';
+import { ContactComponent } from './web-page/contact/contact.component';
+import { AboutComponent } from './web-page/about/about.component';
+import { HeaderComponent } from './web-page/navbar/header/header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SidenavListComponent } from './web-page/navbar/sidenav-list/sidenav-list.component';
+import { AgmCoreModule} from '@agm/core';
+import { MatCarouselModule } from '@ngmodule/material-carousel';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    QuestionsComponent,
+    ContactComponent,
+    AboutComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDpFWj4lMAXPF6su9bX53L1fMs69b0pr10'
+    }),
+    MatCarouselModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
