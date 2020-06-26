6cee4a92c2f886ed03e21bebd35d61212fb01ea3
diana <al234644@edu.uaa.mx>
2020-06-25 23:15:33 -0500

    register

--- src/app/auth/login/register/register.component.ts
+++ src/app/auth/login/register/register.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  form: any;
+  
+  nameFormControl = new FormControl("", [
+    Validators.required,
+    Validators.minLength(4)
+  ]);
 
-  constructor() { }
+ 
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      firstname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      password2: ['', Validators.required],
+
+    });
   }
 
+  ngOnInit(): void {}
 }
