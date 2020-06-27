5cd72492fb3dd0724fa716ccf503322634b5dc51
diana <al234644@edu.uaa.mx>
2020-06-26 20:48:07 -0500

    form-validation

--- src/app/auth/login/register/register.component.ts
+++ src/app/auth/login/register/register.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormControl } from '@angular/forms';
-
+import { AuthService } from '../../service/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
+  providers: [AuthService]
 })
 export class RegisterComponent implements OnInit {
   form: any;
@@ -17,7 +19,7 @@ export class RegisterComponent implements OnInit {
 
  
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,private authSvc: AuthService) {
     this.form = formBuilder.group({
       firstname: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -27,5 +29,39 @@ export class RegisterComponent implements OnInit {
     });
   }
 
+  
+
   ngOnInit(): void {}
+
+  async onRegister()
+  {
+  
+   if(this.form.value.password==this.form.value.password2){
+
+    const {email, password} = this.form.value;
+    try{
+      const user = await this.authSvc.register(email, password);
+      if(user)
+      {
+        Swal.fire({
+          icon: 'success',
+          title: 'El usuario se ha registrado correctamente',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    }catch(error)
+    {
+       
+    }
+   }else{
+    Swal.fire(
+      'Las contraseñas no coinciden',
+      'Vuelve a intentarlo de nuevo',
+      'error'
+    )
+   }
+    
+  }
+
 }
