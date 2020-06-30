63b234b3060bc849ad808121d7f535607dc572f4
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2020-06-30 01:05:53 -0500

    Stadisticas was finished

--- resources/src/app/auth/stadistics/stadistics.component.ts
+++ resources/src/app/auth/stadistics/stadistics.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label } from 'ng2-charts';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-stadistics',
@@ -8,7 +10,7 @@ import { Label } from 'ng2-charts';
   styleUrls: ['./stadistics.component.css']
 })
 export class StadisticsComponent implements OnInit {
-
+  items:Observable<any[]>;
   public barChartOptions: ChartOptions = {
     responsive: true,
     // We use these empty structures as placeholders for dynamic theming.
@@ -20,16 +22,16 @@ export class StadisticsComponent implements OnInit {
       }
     }
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels: Label[] = ['2008', '2009', '2010', '2011', '2012'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    { data: [0, 0, 0, 0, 0], label: 'Enero' },
+    { data: [0, 0, 0, 0, 0], label: 'Febreo' }
   ];
 
-  constructor() { }
+  constructor(private fire:AngularFirestore) {}
 
   ngOnInit() {
   }
@@ -44,16 +46,16 @@ export class StadisticsComponent implements OnInit {
   }
 
   public randomize(): void {
+    let cont:any[];
+    this.items = this.fire.collection('ventas').valueChanges(); 
+    this.items.subscribe( e => {
+      cont = e;
+    });  
     // Only Change 3 values
-    const data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      (Math.random() * 100),
-      56,
-      (Math.random() * 100),
-      40];
-    this.barChartData[0].data = data;
+    setTimeout(e => {
+      this.barChartData[0].data = cont[0].sales;
+      this.barChartData[1].data = cont[1].sales
+    }, 3000);
   }
 
 }
